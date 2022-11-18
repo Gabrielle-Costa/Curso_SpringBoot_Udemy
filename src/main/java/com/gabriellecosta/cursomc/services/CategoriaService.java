ff797133b3b2b8157c778c554b66b99143a55485
Gabrielle-Costa <gabbytargino@yahoo.com.br>
2022-11-18 19:49:37 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/gabriellecosta/cursomc/services/CategoriaService.java
+++ src/main/java/com/gabriellecosta/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.gabriellecosta.cursomc.domain.Categoria;
 import com.gabriellecosta.cursomc.repositories.CategoriaRepository;
+import com.gabriellecosta.cursomc.services.exeptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
 
 }
