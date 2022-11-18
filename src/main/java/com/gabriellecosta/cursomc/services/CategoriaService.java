7cf452479a4e2d59fc93aaa966baccd44825b368
Gabrielle-Costa <gabbytargino@yahoo.com.br>
2022-11-18 17:02:49 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/gabriellecosta/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.gabriellecosta.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriellecosta.cursomc.domain.Categoria;
+import com.gabriellecosta.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired //instancia automaticamente a dependência;
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
