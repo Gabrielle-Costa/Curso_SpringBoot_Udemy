1596c0c74e1031a272382150ba8d664ce74b6f4b
Gabrielle-Costa <gabbytargino@yahoo.com.br>
2022-11-20 20:14:55 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/gabriellecosta/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.gabriellecosta.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriellecosta.cursomc.domain.Pedido;
+import com.gabriellecosta.cursomc.repositories.PedidoRepository;
+import com.gabriellecosta.cursomc.services.exeptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired 
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+
+}
