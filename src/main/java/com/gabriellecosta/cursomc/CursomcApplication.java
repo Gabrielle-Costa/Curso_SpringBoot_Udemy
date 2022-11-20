c2b00f5b66cb964ad2ef23700d59f7b409c7edb5
Gabrielle-Costa <gabbytargino@yahoo.com.br>
2022-11-20 14:23:22 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/gabriellecosta/cursomc/CursomcApplication.java
+++ src/main/java/com/gabriellecosta/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gabriellecosta.cursomc.domain.Categoria;
 import com.gabriellecosta.cursomc.domain.Cidade;
+import com.gabriellecosta.cursomc.domain.Cliente;
+import com.gabriellecosta.cursomc.domain.Endereco;
 import com.gabriellecosta.cursomc.domain.Estado;
 import com.gabriellecosta.cursomc.domain.Produto;
+import com.gabriellecosta.cursomc.domain.enums.TipoCliente;
 import com.gabriellecosta.cursomc.repositories.CategoriaRepository;
 import com.gabriellecosta.cursomc.repositories.CidadeRepository;
+import com.gabriellecosta.cursomc.repositories.ClienteRepository;
+import com.gabriellecosta.cursomc.repositories.EnderecoRepository;
 import com.gabriellecosta.cursomc.repositories.EstadoRepository;
 import com.gabriellecosta.cursomc.repositories.ProdutoRepository;
 
@@ -28,6 +33,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -67,7 +76,27 @@ public class CursomcApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+	
+		cli1.getTelefones().addAll(Arrays.asList("27363323","993838393"));
 		
+		Endereco e1 = new Endereco(null,"RuaFlores","300","Apto 203","Jardim","3822,834",cli1,c1);
+		Endereco e2 = new Endereco(null,"Avenida Matos", "105","Sala 800","Centro","38777012",cli1,c2);
+	
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
--- src/main/java/com/gabriellecosta/cursomc/domain/Categoria.java
+++ src/main/java/com/gabriellecosta/cursomc/domain/Categoria.java
@@ -17,8 +17,6 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
